c169f4e1e7a77f2882addbbef318289bf399930d
Riomus <riomus@gmail.com>
2014-05-24 17:16:34 +0200

    Speed fix?

--- progfun-objsets/src/main/scala/objsets/TweetSet.scala
+++ progfun-objsets/src/main/scala/objsets/TweetSet.scala
@@ -136,7 +136,7 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
   
   
-  def union(that: TweetSet): TweetSet =right.union(left.union(that)).incl(elem);
+  def union(that: TweetSet): TweetSet =left.union(right.union(that.incl(elem)));
     
   
   
